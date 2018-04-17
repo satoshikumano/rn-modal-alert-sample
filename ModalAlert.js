475389ceb1b8267f55cdcf7bdc8c40f2434823c6
satoshi <satoshi.kumano@kii.com>
2018-04-17 12:51:40 +0900

    Initial commit.

---
+++ ModalAlert.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Modal,
+  Text,
+  View,
+  Button
+} from 'react-native';
+
+export default class ModalAlert extends Component<{}> {
+  state = {
+    modalVisible: false,
+  };
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
+  render() {
+    return (
+      <View style={{marginTop: 22}}>
+        <Modal
+          animationType="none"
+          transparent={true}
+          visible={this.state.modalVisible}
+        >
+          <View
+            style={styles.alertContainer}
+          >
+            <View style={styles.alertView}>
+              <View style={styles.alertContentView}>
+                <Text>Sample Alert</Text>
+                <Button
+                  onPress={() => {this.setModalVisible(false)}}
+                  title="OK">
+                </Button>
+              </View>
+            </View>
+          </View>
+        </Modal>
+        <Button
+          style={{marginTop: 22}}
+          onPress={() => {this.setModalVisible(true)}}
+          title="Show Alert">
+        </Button>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  alertContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(52, 52, 52, 0.8)'
+  },
+  alertView: {
+    width: 300,
+    height: 150,
+    backgroundColor: 'white',
+    borderStyle: 'solid',
+    borderRadius: 5
+  },
+  alertContentView: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
